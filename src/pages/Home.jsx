7b177caca169b7b0b567d800ff87601eaec409bd
Andy Hua <user.andy.hua@gmail.com>
2023-01-05 00:01:05 -0800

    second

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    return <div>Home</div>;
+    const [airing, setAiring] = useState([]);
+    useEffect(() => {
+        fetch(`https://api.jikan.moe/v4/schedules`)
+            .then(data => data.json())
+            .then(data => setAiring(data.data));
+    }, [airing]);
+
+    return (
+        <div>
+            {airing?.map(x => (
+                <>
+                    <img src={x.images.jpg.large_image_url} alt={x.title} />
+                    <h1>{x.title_english || x.title}</h1>
+                </>
+            ))}
+        </div>
+    );
 };
 
 export default Home;
